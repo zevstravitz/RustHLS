b8d37165c28564e41f63d972bbbf878277853f9d
Neil Vakharia <neilvakharia@Neils-Air.attlocal.net>
2021-05-21 14:00:52 -0500

    simple server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+extern crate gotham;
+
+use gotham::handler::assets::FileOptions;
+use gotham::router::builder::{build_simple_router, DefineSingleRoute, DrawRoutes};
+
+pub fn main() {
+    let path = std::env::args()
+        .nth(1)
+        .expect("Need to pass an arg which is the path to serve");
+    let addr = "127.0.0.1:7878";
+    println!(
+        "Listening for requests at http://{} from path {:?}",
+        addr, path
+    );
+
+    let router = build_simple_router(|route| {
+        route.get("/demo.m3u8").to_file("assets/demo.m3u8");
+    });
+
+    gotham::start(addr, router)
 }
